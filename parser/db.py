fe52d2b2c27af2a2cf825057b370cd8ce25119c9
Igor4er <sherednihenko.ihor@gmail.com>
2022-08-02 22:14:05 +0300

    Прискорено швидкість роботи функції update_specs()

--- parser/db.py
+++ parser/db.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from config import DB_USER, DB_PASSWORD
 from console import console, Progress
+from time import sleep
 
 
 def connect_to_db():
@@ -36,18 +37,21 @@ def get_collection(vst, collection):
 def update_specs(vst, spec_list):
     def _insert():
         with Progress() as prog:
-            task = prog.add_task("[dodger_blue2]Оновлення...", total=len(spec_list))
+            task = prog.add_task("[light_sea_green]Оновлення...", total=len(spec_list)+3)
+            sl = []
             for spec in spec_list:
                 spe = {"code": spec[0], "name": spec[1]}
-                specs.insert_one(spe)
+                sl += [spe]
+                # specs.insert_one(spe)
+                sleep(0.002)
                 prog.update(task, advance=1)
+            specs.insert_many(sl)
+            prog.update(task, advance=3)
     console.print("Оновлення списку спеціальностей... ", end="")
     if "specs" in vst.list_collection_names():
         vst["specs"].drop()
         specs = vst["specs"]
-        console.print(f"[black on blue]ПЕРЕДАНО[/]")
-        _insert()
     else:
         specs = vst["specs"]
-        console.print(f"[black on blue]ПЕРЕДАНО[/]")
-        _insert()
+    console.print(f"[black on blue]ПЕРЕДАНО[/]")
+    _insert()
