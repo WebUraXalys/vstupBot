74c0615b87071897ac91beb75dc8fbbe0209195a
writer-mng <volodymyrpetriv2207@gmail.com>
2022-08-03 10:59:57 +0300

    Підготовка до роботи з mongoDB

--- bot/main_bot.py
+++ bot/main_bot.py
@@ -1,7 +1,7 @@
 import os
 import pymongo
 from aiogram import Bot, Dispatcher, executor, types
-
+from urllib.parse import quote_plus
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,12 +10,26 @@ bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot)
 
 # Preparing for using MongoDB
-client = os.getenv("pymongo_client")
+# Є ще проблема з підключенням до бази. Буду вирішувати
+# pymongo.errors.InvalidURI: Username and password must be escaped according to RFC 3986, use urllib.parse.quote_plus
+client = pymongo.MongoClient("Адресу так дам, не буду ж пароль зливати", tls=True, tlsAllowInvalidCertificates=True)
+db = client.vst
+coll = db.specs
 
 
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
     await message.reply("Привіт.")
+
+
+@dp.message_handler(commands="get")
+async def get_data(message: types.Message):
+    # Наразі функціональності не має, циклом хочу перевірити, як працює pymongo
+    c = 0
+    if c<10:
+        for value in coll.find():
+            await message.reply(value)
+            c += 1
 
 
 @dp.message_handler(commands="help")
