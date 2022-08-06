5da83c5542e76ac75309b10bcfe8c03a33d7ec1c
writer-mng <volodymyrpetriv2207@gmail.com>
2022-08-06 14:57:39 +0300

    Завершено роботу зі збору даних

--- bot/main_bot.py
+++ bot/main_bot.py
@@ -9,6 +9,7 @@ load_dotenv()
 
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot)
+bots_username = "@test_payment_25435_bot"
 
 # Preparing for using MongoDB
 username = os.getenv('user_name')
@@ -40,7 +41,7 @@ async def check_profession_step_2(message: types.Message):
     if message.text == "Ні":
         await message.answer(
             'Рекомендуємо тобі пройти <a href="https://quizterra.com/ru/kto-ty-iz-vinks">цей тест</a>',
-            parse_mode="HTML", disable_web_page_preview=True)
+            parse_mode="HTML", disable_web_page_preview=True, reply_markup=types.ReplyKeyboardRemove())
         await asyncio.sleep(3)
         await message.answer("Тепер ти знаєш, ким хочеш бути?", reply_markup=nav.check_profession_step_2, disable_notification=True)
 
@@ -48,21 +49,20 @@ async def check_profession_step_2(message: types.Message):
         await message.answer(
             "Тепер потрібно написати код або коди спеціальностей чи вибрати із списку", reply_markup=nav.specialization)
     else:
-        await message.answer("Порадься з батьками, чи що")
+        await message.answer("Порадься з батьками, чи що", reply_markup=types.ReplyKeyboardRemove())
 
 
 @dp.callback_query_handler(text="спеціальність")
-async def specialnist(call: types.CallbackQuery):
+async def auto_specialization(call: types.CallbackQuery):
     await call.answer()
-    await call.message.answer("Заїбусь з цим точно")
+    # await call.message.answer("Вибери спеціальність", reply_markup=nav.choose_category)
     await call.message.answer(call.message.message_id)
 
 
 @dp.callback_query_handler(text="код")
-async def specialnist(call: types.CallbackQuery):
+async def specialization(call: types.CallbackQuery):
     await call.answer()
-    await call.message.answer("Пропиши команду /set_code, а тоді через пробіл перечисли номери спеціальностей.\n Приклад: /set_code 011 121 125 123")
-    await call.message.answer(call.message.message_id)
+    await call.message.answer("Пропиши команду /setcode, а тоді через пробіл перечисли номери спеціальностей.\n Приклад: /setcode 011 121 125 123", reply_markup=types.ReplyKeyboardRemove())
 
 
 @dp.message_handler(commands="setcode")
@@ -77,7 +77,7 @@ async def set_code(message: types.Message):
         finder = coll.find({'name': {"$regex": value}}, {'_id': 0, "name": 1})
         c = []
         for f in finder:
-            c.append(coll)
+            c.append(f)
         if len(c) == 1:
             spec_codes.append(value)
         elif len(c) < 1:
@@ -90,48 +90,143 @@ async def set_code(message: types.Message):
     for r in res:
         result.append(r)
     if len(result) < 1:
-        await message.answer("Нема запису")
         db.users_specs.insert_one({'user_id': user_id,
-                                   'spec_codes': spec_codes})
+                                   'spec_codes': spec_codes,
+                                   'UA': "",
+                                   'Math': "",
+                                   "History": ""})
         await message.answer("Запис внесено до бази даних")
-        await message.answer(f"Вибрано спеціальності: {spec_codes}")
+        await message.answer(f"Вибрано спеціальності: {spec_codes}", reply_markup=nav.cont)
     elif len(result) == 1:
         db.users_specs.update_one({'user_id': user_id}, {'$set': {'spec_codes': spec_codes}})
         await message.answer("Запис в базі даних оновлено")
-        await message.answer(f"Вибрано спеціальності: {spec_codes}")
+        await message.answer(f"Вибрано спеціальності: {spec_codes}", reply_markup=nav.cont)
 
 
-
-@dp.message_handler(commands="get_id")
-async def get_id(message: types.Message):
-    await message.answer(message.reply_to_message.message_id)
-
-
-@dp.message_handler(commands="get")
-async def get_data(message: types.Message):
-    # Наразі функціональності не має, циклом хочу перевірити, як працює pymongo
-    c = 0
-    for value in coll.find():
-        if c < 10:
-            await message.answer(value["code"] + " " + value["name"])
-            await asyncio.sleep(0.05)
-            c += 1
+@dp.message_handler(lambda message: message.text == "Продовжити далі")
+async def choose_region(message: types.Message):
+    await message.answer("З спеціальностями покінчено, час перейти до наступного етапу", reply_markup=types.ReplyKeyboardRemove())
+    await asyncio.sleep(1.5)
+    db.users_specs.update_one({"user_id": message.from_user.id}, {"$unset": {"region": ""}})
+    await message.answer("Вам потрібно вибрати одну або кілька областей, де ви хотіли б навчатись. Щоб скасувати вибір області потрібно повторно натиснути на її кнопку", reply_markup=nav.regions)
 
 
-@dp.message_handler(commands="help")
-async def help_user(message: types.Message):
-    await message.reply(
-        "Вас вітає vstupBot. Я створений, щоб допомогти абітурієнтам обрати найкращий університет для вступу на бюджет.")
+@dp.callback_query_handler(text_contains="region")
+async def add_or_remove_region(call: types.CallbackQuery):
+    await call.answer()
+    user_id = call.from_user.id
+    data_split = call.data.split()
+    region_name = data_split[1]
+    if region_name == "АР":
+        region_name = "АР Крим"
+
+    res = db.users_specs.find_one({'user_id': user_id})
+    try:
+        if region_name not in res["region"]:
+            print(region_name)
+            db.users_specs.update_one({'user_id': user_id}, {"$push": {"region": region_name}})
+            if region_name == "Київ" or region_name == "АР Крим":
+                await call.message.answer(f"Додано регіон: {region_name}")
+            else:
+                await call.message.answer(f"Додано регіон: {region_name} область")
+
+        else:
+            db.users_specs.update_one({'user_id': user_id}, {"$pull": {"region": region_name}})
+            if region_name == "Київ" or region_name == "АР Крим":
+                await call.message.answer(f"Видалено регіон: {region_name}")
+            else:
+                await call.message.answer(f"Видалено регіон: {region_name} область")
+    except KeyError:
+        db.users_specs.update_one({'user_id': user_id}, {"$push": {"region": region_name}})
+        if region_name == "Київ" or region_name == "АР Крим":
+            await call.message.answer(f"Додано регіон: {region_name}")
+        else:
+            await call.message.answer(f"Додано регіон: {region_name} область")
 
 
-@dp.message_handler(commands="report")
-async def send_report(message: types.Message):
-    await message.reply("Відправлятиметься репорт")
+@dp.callback_query_handler(text_contains="The end")
+async def get_exams_result(call: types.CallbackQuery):
+    await call.answer()
+    if "repeat" not in call.data.split():
+        mess = "Готово. Ви вибрали наступні регіони:\n"
+        regions = db.users_specs.find_one({"user_id": call.from_user.id}, {"_id": 0, "region": 1})
+        region_sort = []
+        for region in regions["region"]:
+            region_sort.append(region)
+        region_sort.sort()
+        for region in region_sort:
+            if region == "АР Крим" or region == "Київ":
+                mess += f"✅{region}\n"
+            else:
+                mess += f"✅{region} область\n"
+        await call.message.answer(mess)
+    await call.message.answer("Тепер потрібно ввести бали з екзаменів. Наразі доступно лише три предмета, які були на НМТ: українська мова, математика та історія."
+                              "Почергово нажміть кожну з кнопок та відповіддю введіть свій бал. Опісля поверніться до цього "
+                              "повідомлення та натисність кнопку \"Зберегти\" \n\n",
+                              reply_markup=nav.examsMenu)
 
 
-@dp.message_handler(commands="pay")
-async def payment(message: types.Message):
-    await message.reply("Я проситиму гроші, коли отримаю картку")
+@dp.callback_query_handler(text_contains="exam")
+async def get_exams_result(call: types.CallbackQuery):
+    await call.answer()
+    exam_mark = call.data.split()
+    index = exam_mark[1]
+    if index == "ua":
+        await call.message.answer("Введіть бал з української мови")
+    elif index == "math":
+        await call.message.answer("Введіть бал з математики")
+    elif index == "history":
+        await call.message.answer("Введіть бал з історії")
+
+
+@dp.message_handler(lambda message: int(message.text) in range(-1000, 1000))
+async def push_exam(message: types.Message):
+    if not message.reply_to_message:
+        await message.answer("Потрібно відправити як відповідь на повідомлення")
+
+    elif message.reply_to_message:
+        correct = True
+        if int(message.text) != 0 and int(message.text) <= 200:
+            separated_text = message.reply_to_message.text.split()
+            user_id = message.from_user.id
+            if "української" in separated_text:
+                await message.answer(f"Бал з української мови: {message.text}")
+                db.users_specs.update_one({"user_id": user_id}, {"$set": {"UA": int(message.text)}})
+            if "математики" in separated_text:
+                await message.answer(f"Бал з математики: {message.text}")
+                db.users_specs.update_one({"user_id": user_id}, {"$set": {"Math": int(message.text)}})
+            if "історії" in separated_text:
+                await message.answer(f"Бал з історії: {message.text}")
+                db.users_specs.update_one({"user_id": user_id}, {"$set": {"History": int(message.text)}})
+
+        else:
+            correct = False
+            if int(message.text) <= 0:
+                await message.answer("Бал не може дорівнювати нулю або бути меншим за нього")
+            else:
+                await message.answer("У вас прекрасні досягнення. Мабуть, вам не потрібна наша допомога, якщо у вас оцінка в більш ніж 200 балів")
+
+    if correct:
+        pass
+    else:
+        await message.answer("Помилка в балах. Виправте свої бали")
+
+
+@dp.callback_query_handler(text="save")
+async def save(call: types.CallbackQuery):
+    await call.answer()
+    user_coll = db.users_specs.find_one({"user_id": call.from_user.id})
+
+    ua = user_coll["UA"]
+    hst = user_coll["History"]
+    math = user_coll["Math"]
+
+    await call.message.answer(
+        "Введені оцінки \n\n"
+        f"Українська мова: {ua},\nІсторія: {hst},\nМатематика: {math}\n\n"
+        "Якщо ви помилилися, нажміть кнопку", reply_markup=nav.save)
+    await asyncio.sleep(5)
+    await call.message.answer("Налаштування завершено")
 
 
 if __name__ == "__main__":
--- bot/markup.py
+++ bot/markup.py
@@ -1,6 +1,5 @@
 from aiogram import types
 
-
 guideBTN = [
         types.InlineKeyboardButton(text="Текстовий гайд", url="google.com", callback_data="trusted"),
         types.InlineKeyboardButton(text="Відео гайд", url="youtube.com", callback_data="trusted"),
@@ -31,3 +30,49 @@ specialization = types.InlineKeyboardMarkup()
 specBTN = [types.InlineKeyboardButton(text="Ввести код", callback_data="код"),
            types.InlineKeyboardButton(text="Спеціальність", callback_data="спеціальність")]
 specialization.add(*specBTN)
+
+
+cont = types.ReplyKeyboardMarkup(resize_keyboard=True)
+cont.add("Продовжити далі")
+
+
+regions = types.InlineKeyboardMarkup(row_width=2)
+regionBTN = [types.InlineKeyboardButton(text="АР Крим", callback_data="region АР Крим"),
+             types.InlineKeyboardButton(text="Вінницька", callback_data="region Вінницька"),
+             types.InlineKeyboardButton(text="Волинська", callback_data="region Волинська"),
+             types.InlineKeyboardButton(text="Дніпропетровська", callback_data="region Дніпропетровська"),
+             types.InlineKeyboardButton(text="Донецька", callback_data="region Донецька"),
+             types.InlineKeyboardButton(text="Житомирська", callback_data="region Житомирська"),
+             types.InlineKeyboardButton(text="Закарпатська", callback_data="region Закарпатська"),
+             types.InlineKeyboardButton(text="Івано-Франківська", callback_data="region Івано-Франківська"),
+             types.InlineKeyboardButton(text="Київська", callback_data="region Київська"),
+             types.InlineKeyboardButton(text="Кіровоградська", callback_data="region Кіровоградська"),
+             types.InlineKeyboardButton(text="Луганська", callback_data="region Луганська"),
+             types.InlineKeyboardButton(text="Львівська", callback_data="region Львівська"),
+             types.InlineKeyboardButton(text="Миколаївська", callback_data="region Миколаївська"),
+             types.InlineKeyboardButton(text="Одеська", callback_data="region Одеська"),
+             types.InlineKeyboardButton(text="Полтавська", callback_data="region Полтавська"),
+             types.InlineKeyboardButton(text="Рівненська", callback_data="region Рівненська"),
+             types.InlineKeyboardButton(text="Сумська", callback_data="region Сумська"),
+             types.InlineKeyboardButton(text="Тернопільська", callback_data="region Тернопільська"),
+             types.InlineKeyboardButton(text="Харківська", callback_data="region Харківська"),
+             types.InlineKeyboardButton(text="Херсонська", callback_data="region Херсонська"),
+             types.InlineKeyboardButton(text="Хмельницька", callback_data="region Хмельницька"),
+             types.InlineKeyboardButton(text="Черкаська", callback_data="region Черкаська"),
+             types.InlineKeyboardButton(text="Чернівецька", callback_data="region Чернівецька"),
+             types.InlineKeyboardButton(text="Чернігівська", callback_data="region Чернігівська"),
+             types.InlineKeyboardButton(text="Київ", callback_data="region Київ"),
+             types.InlineKeyboardButton(text="Завершити обирати регіон", callback_data="The end"),
+             ]
+regions.add(*regionBTN)
+
+examsMenu = types.InlineKeyboardMarkup(row_width=1)
+examsBTN = [types.InlineKeyboardButton(text="Українська мова", callback_data="exam ua"),
+            types.InlineKeyboardButton(text="Математика", callback_data="exam math"),
+            types.InlineKeyboardButton(text="Історія", callback_data="exam history"),
+            types.InlineKeyboardButton(text="Зберегти", callback_data="save")
+            ]
+examsMenu.add(*examsBTN)
+
+save = types.InlineKeyboardMarkup()
+save.add(types.InlineKeyboardButton(text="Виправити помилку", callback_data="The end repeat"))
